897860bcb15724c9470f492b1d9ef44f0308a4b7
Shawn <shawn@mag.cr>
2021-06-21 10:17:10 +0200

    implemented threejs and animations

--- components/Intro/index.js
+++ components/Intro/index.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import _ from './style.module.css'
+import Icon from '../Icon'
 
-const Intro = () => (
-  <section className={_.intro} id='intro'>
-    <h2>My frontend journey so far</h2>
-    <p>I have always had an interest in websites, how they look and function. I have been freelancing since 2016 building basic Wordpress websites on side. It was until a couple of years ago that my frontend web developer journey sky rocketed when a friend started mentoring me and guided me in developing solid skills in html, css and javascript.
-    </p>
+const Intro = () => {
+
+  const [readmore, setReadmore] = useState(false);
+
+  const more = <div className={_.more}>
     <p>I started working at Magnetic Creative in 2019 as a junior front end web developer. The tech stack was primarily custom Wordpress dockerised with postcss and ACF for customising the backend for customers. The languages I worked with was php, html, postscss, javascript. The highlight was working with a team of developers and utilising version control via GitHub.</p>
     <p>In 2021 I took a break from agency life and joined a startup called Orderin as front end web developer. I developed valuable skills in REACT.js, working with Gatsby.js, next.js, storybook and storyblok. The highlight was developing a design system for orderin with REACT and typescript.</p>
-  </section>
-)
+  </div>
+
+  return (
+    <section className={_.intro} id='intro'>
+      <h2>My frontend journey so far</h2>
+      <p>I have always had an interest in websites, how they look and function. I have been freelancing since 2016 building basic Wordpress websites on side. It was until a couple of years ago that my frontend web developer journey sky rocketed when a friend started mentoring me and guided me in developing solid skills in html, css and javascript.
+      </p>
+      {readmore && more}
+      <span>{readmore ? 'Read Less' : 'Read More...'}</span>
+      <a className={`${readmore ? _.open : ''}`} onClick={() => { setReadmore(!readmore) }}><Icon name='arrowdown' /></a>
+    </section>
+  )
+}
 export default Intro
